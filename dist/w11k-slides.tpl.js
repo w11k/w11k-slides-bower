cc4fa5e65d6e42159e41bb5e8e2443845b61e1bb
Philipp Burgmer <burgmer@weiglewilczek.com>
2015-03-03 01:32:04 +0100

    release 0.10.0

--- dist/w11k-slides.tpl.js
+++ dist/w11k-slides.tpl.js
@@ -1,6 +1,11 @@
-angular.module('w11k.slides.template', ['slides/slides.tpl.html']);
+angular.module('w11k.slides.template', ['slides/slides.tpl.html', 'footer/footer.tpl.html']);
 
 angular.module('slides/slides.tpl.html', []).run(['$templateCache', function($templateCache) {
   $templateCache.put('slides/slides.tpl.html',
-    '<div class="slides export" ng-controller="SlidesCtrl"><div class="overlay"></div><div ng-repeat="slide in slides" ng-show="slide.active"><div ng-include="slide.template"></div></div></div>');
+    '<div class="slides export" ng-controller="SlidesCtrl"><div class="overlay"></div><div class="slide-container" ng-repeat="slide in slides" ng-show="slide.active"><div ng-include="slide.template"></div></div></div>');
+}]);
+
+angular.module('footer/footer.tpl.html', []).run(['$templateCache', function($templateCache) {
+  $templateCache.put('footer/footer.tpl.html',
+    '<div class="w11k-footer"><div class="row"><div class="col-sm-3 footer-left"></div><div class="col-sm-6 footer-middle"></div><div class="col-sm-3 footer-right"></div></div></div>');
 }]);
