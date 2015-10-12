b8414167445e13e80eaad16785b75437b92e4e60
Philipp Burgmer <burgmer@w11k.de>
2015-10-12 20:11:19 +0200

    release 0.12.1

--- dist/w11k-slides.tpl.js
+++ dist/w11k-slides.tpl.js
@@ -6,21 +6,21 @@
  * @license MIT
  */
 (function(module) {
-try { module = angular.module("w11k.slides.template"); }
-catch(err) { module = angular.module("w11k.slides.template", []); }
-module.run(["$templateCache", function($templateCache) {
-  "use strict";
-  $templateCache.put("src/footer/footer.html",
-    "<div class=w11k-footer><div class=row><div class=\"col-sm-3 footer-left\"></div><div class=\"col-sm-6 footer-middle\"></div><div class=\"col-sm-3 footer-right\"></div></div></div>");
+try { module = angular.module('w11k.slides.template'); }
+catch(err) { module = angular.module('w11k.slides.template', []); }
+module.run(['$templateCache', function($templateCache) {
+  'use strict';
+  $templateCache.put('footer/footer.html',
+    '<div class=w11k-footer><div class=row><div class="col-sm-3 footer-left"></div><div class="col-sm-6 footer-middle"></div><div class="col-sm-3 footer-right"></div></div></div>');
 }]);
 })();
 
 (function(module) {
-try { module = angular.module("w11k.slides.template"); }
-catch(err) { module = angular.module("w11k.slides.template", []); }
-module.run(["$templateCache", function($templateCache) {
-  "use strict";
-  $templateCache.put("src/slides/slides.html",
-    "<div class=\"slides export\" ng-controller=\"SlidesCtrl as sc\"><div class=overlay></div><div class=slide-container ng-repeat=\"slide in sc.slides\" ng-show=slide.active><div ng-include=slide.template></div></div></div>");
+try { module = angular.module('w11k.slides.template'); }
+catch(err) { module = angular.module('w11k.slides.template', []); }
+module.run(['$templateCache', function($templateCache) {
+  'use strict';
+  $templateCache.put('slides/slides.html',
+    '<div class="slides export" ng-controller="SlidesCtrl as sc"><div class=overlay></div><div class=slide-container ng-repeat="slide in sc.slides" ng-show=slide.active><div ng-include=slide.template></div></div></div>');
 }]);
 })();
